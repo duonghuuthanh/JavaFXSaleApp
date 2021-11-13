df3a7950fff2bd4a80256d4e2948bfd9722afe47
Thanh Duong <dhthanhqa@gmail.com>
2021-11-13 08:34:49 +0700

    fix bugs

--- MyAppV2/src/main/java/com/dht/myappv2/Demo.java
+++ MyAppV2/src/main/java/com/dht/myappv2/Demo.java
@@ -14,6 +14,7 @@ public class Demo {
         System.out.println("Hello world 2!!!");
         System.out.println("TEST");
         int a = 10, b = 20;
-        System.out.println(a + b);
+        if (a > 0)
+            System.out.println(a + b);
     }
 }
